e7783ee24f0b36c07683c261d0fc6e13667ec663
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2022-07-01 21:22:49 +0200

    Trabajando con Refs y Forward Refs

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,15 +1,17 @@
+import React from 'react';
+
 import classes from './Input.module.css';
 
-const Input = (props) => {
+const Input = React.forwardRef((props, ref) => {
   return (
     <div className={classes.input}>
       <label htmlFor={props.input.id}>{props.label}</label>
       {/* Si props.input tiene una clave-valor, por ejemplo {type: 'text'}  
           con {...props.input} nos aseguramos que lo reciba. 
           Este truco lo hace muy configurable */}
-      <input {...props.input} />
+      <input ref={ref} {...props.input} />
     </div>
   );
-};
+});
 
 export default Input;
